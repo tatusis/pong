a7989d3ad292038d3ef54266021859a2453ab179
tatusis <rafaelsferreira@icloud.com>
2025-10-12 15:34:45 -0300

    Makes the goal area safer

--- src/main/goal.py
+++ src/main/goal.py
@@ -15,14 +15,14 @@ class Goal(pygame.sprite.Sprite):
         self.screen = screen
         self.side = side
         self.image = pygame.surface.Surface(
-            (self.settings["goal.size"], self.screen.get_rect().height - (self.settings["goal.size"] * 2))
+            (self.settings["goal.size"], self.screen.get_rect().height + (self.settings["goal.size"] * 4))
         )
         self.image.fill(pygame.color.THECOLORS[self.settings["goal.color"]])
         self.rect = self.image.get_rect()
 
         if self.side == Side.LEFT:
-            self.rect.top = self.screen.get_rect().top + self.settings["goal.size"]
+            self.rect.top = self.screen.get_rect().top - (self.settings["goal.size"] * 2)
             self.rect.left = self.screen.get_rect().left - (self.settings["goal.size"] * 2)
         elif self.side == Side.RIGHT:
-            self.rect.top = self.screen.get_rect().top + self.settings["goal.size"]
+            self.rect.top = self.screen.get_rect().top - (self.settings["goal.size"] * 2)
             self.rect.right = self.screen.get_rect().right + (self.settings["goal.size"] * 2)
